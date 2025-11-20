667703c9ae29b5d007e41626dcb70b89e3962a5e
mohammednoumaanhussain-cyber <mohammednoumaanhussain@gmail.com>
2025-11-20 15:17:58 +0530

    day 4 code pushed

---
+++ Day 4/2_variables.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+    int age = 30;
+    printf("your age is: %d", age);
+
+    float marks=345.67;
+    printf("\nyour marks is: %.2f",marks);
+
+    double pi=3.14;
+    printf("\nPi value is: %lf",pi);
+
+    char name= 'H';
+    printf("\nYour \? namr is: %c",name);
+    bool iswalking=false;
+    printf("\nis\twalking: %d",iswalking);
+
+}
